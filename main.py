0f4f754df4e37c7a77f10914473714c6cf979435
Yoriichi-arch <marvdeadpool2373@gmail.com>
2022-12-20 21:15:16 +0500

    First version

---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.cell_size = 80
+        self.left = 1000 // 2 - (self.cell_size * 6) // 2
+        self.top = 700 // 2 - (self.cell_size * 6) // 2
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, surface):
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                pygame.draw.rect(surface, 'white',
+                                 (self.left + c * self.cell_size,
+                                  self.top + r * self.cell_size,
+                                  self.cell_size, self.cell_size), 1)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def get_cell(self, mouse_coord):
+        x, y = mouse_coord
+        r, c = (y - self.top) // self.cell_size, (x - self.left) // self.cell_size
+        if 0 <= r <= self.height and 0 <= c < self.width:
+            return r, c
+        return None
+
+    @staticmethod
+    def on_click(cell):
+        print(cell)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 700))
+    screen.fill('grey')
+    board = Board(6, 6)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        board.render(screen)
+        pygame.display.flip()
